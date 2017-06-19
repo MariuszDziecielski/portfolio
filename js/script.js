34ece347d82e2bf6f5f198b4f7ae1e6173cefe17
Mariusz Dziecielski <mariuszdziec@gmail.com>
2017-06-19 20:56:27 +0200

    Change html, js and sass

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 $(function () {
+    AOS.init({
+        offset: 200,
+        duration: 600,
+        easing: 'ease-in-sine',
+        delay: 300,
+        disable: $(window).width() < 768
+    });
     $('header li').each(function (index, elem) {
         $(elem).click(function () {
             $('header li').removeClass('active');
@@ -10,4 +17,22 @@ $(function () {
             $('.navbar-toggle').click();
         }
     });
+    $('.navbar-nav li a').each(function (index, elem) {
+        $(elem).click(function () {
+            var href = $(elem).attr('href');
+            $(href).animatescroll({
+                scrollSpeed: 4000,
+                easing: 'easeOutBack',
+                padding: 40
+            });
+            AOS.refreshHard();
+        });
+    });
+    if ($(window).width() < 1196) {
+        $('.projects .chart').removeAttr('data-aos-delay');
+    }
+    if ($(window).width() < 1000) {
+        $('.known_technologies .chart').removeAttr('data-aos-delay');
+        $('.unknown_technologies .chart').removeAttr('data-aos-delay');
+    }
 });
