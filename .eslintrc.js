8bdabdcc07ccdcbf66904063bd84936c9c462897
Mariusz Dziecielski <mariuszdziec@gmail.com>
2018-06-14 07:20:05 +0200

    Change images source & add two known technologies

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "jquery": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": 0,
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
--- docs/js/script.js
+++ docs/js/script.js
@@ -1,22 +1,22 @@
 const $listItems = $('header li');
-$listItems.each((index, elem) => {
-    $(elem).click(() => {
-        $listItems.removeClass('active');
-        $(elem).addClass('active');
-    });
+$listItems.each((i, elem) => {
+	$(elem).click(() => {
+		$listItems.removeClass('active');
+		$(elem).addClass('active');
+	});
 });
 $('.navbar-collapse a').click(() => {
-    if ($('.navbar-collapse').hasClass('in')) {
-        $('.navbar-toggle').click();
-    }
+	if ($('.navbar-collapse').hasClass('in')) {
+		$('.navbar-toggle').click();
+	}
 });
-$('.navbar-nav li a').each((index, elem) => {
-    $(elem).click(() => {
-        const href = $(elem).attr('href');
-        $(href).animatescroll({
-            scrollSpeed: 4000,
-            easing: 'easeOutBack',
-            padding: 40
-        });
-    });
+$('.navbar-nav li a').each((i, elem) => {
+	$(elem).click(() => {
+		const href = $(elem).attr('href');
+		$(href).animatescroll({
+			scrollSpeed: 4000,
+			easing: 'easeOutBack',
+			padding: 40
+		});
+	});
 });
