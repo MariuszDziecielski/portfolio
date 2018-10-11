8d4be92638c1f51a264627416631dd70a290a0c4
Mariusz Dziecielski <mariuszdziec@gmail.com>
2018-10-11 17:21:20 +0200

    Change project thumbnail & update some packages

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,72 +1,72 @@
-module.exports = function (grunt) {
-    // Project configuration
-    grunt.initConfig({
-        sass: {
-            options: {
-                sourceMap: true,
-                outputStyle: 'expanded'
-            },
-            dist: {
-                files: {
-                    'css/style.css': 'sass/style.sass'
-                }
-            }
-        },
-        imagemin: {
-            dynamic: {
-                files: [{
-                    expand: true,
-                    cwd: 'img/',
-                    src: ['**/*.{png,jpg,gif}'],
-                    dest: 'images/'
-                }]
-            }
-        },
-        jshint: {
-            all: ['Gruntfile.js', 'js/script.js'],
-            options: {
-                'esversion': 6
-            }
-        },
-        watch: {
-            sass: {
-                files: ['sass/*.sass'],
-                tasks: ['sass'],
-                options: {
-                    spawn: false
-                }
-            },
-            jshint: {
-                files: ['Gruntfile.js', 'js/script.js'],
-                tasks: ['jshint'],
-                options: {
-                    spawn: false
-                }
-            }
-        },
-        browserSync: {
-            dev: {
-                bsFiles: {
-                    src : [
-                        'css/*.css',
-                        'js/*.js',
-                        '*.html'
-                    ]
-                },
-                options: {
-                    watchTask: true,
-                    server: true,
-                    browser: ['chrome']
-                }
-            }
-        }
-    });
-    // Load the plugins tasks
-    grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-browser-sync');
-    // Default task(s)
-    grunt.registerTask('default', ['imagemin', 'sass', 'jshint', 'browserSync', 'watch']);
+'use strict';
+
+module.exports = grunt => {
+	const sass = require('node-sass');
+
+	require('load-grunt-tasks')(grunt);
+
+	grunt.initConfig({
+		sass: {
+			options: {
+				implementation: sass,
+				sourceMap: true,
+				outputStyle: 'expanded'
+			},
+			dist: {
+				files: {
+					'css/style.css': 'sass/style.sass'
+				}
+			}
+		},
+		imagemin: {
+			dynamic: {
+				files: [{
+					expand: true,
+					cwd: 'img/',
+					src: ['**/*.{png,jpg,gif}'],
+					dest: 'images/'
+				}]
+			}
+		},
+		jshint: {
+			all: ['js/script.js'],
+			options: {
+				'esversion': 6
+			}
+		},
+		watch: {
+			sass: {
+				files: ['sass/*.sass'],
+				tasks: ['sass'],
+				options: {
+					spawn: false
+				}
+			},
+			jshint: {
+				files: ['js/script.js'],
+				tasks: ['jshint'],
+				options: {
+					spawn: false
+				}
+			}
+		},
+		browserSync: {
+			dev: {
+				bsFiles: {
+					src: [
+						'css/*.css',
+						'js/*.js',
+						'*.html'
+					]
+				},
+				options: {
+					watchTask: true,
+					server: true,
+					browser: ['chrome']
+				}
+			}
+		}
+	});
+
+	grunt.registerTask('default', ['imagemin', 'sass', 'jshint', 'browserSync', 'watch']);
 };
